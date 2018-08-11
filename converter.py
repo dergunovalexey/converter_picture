c162ab53bc55dffe6be0886b547385b1d20a1e58
Alex <dergunovalexey2000@gmail.com>
2018-08-11 12:45:22 +0700

    update converter

--- converter.py
+++ converter.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python
 import imageio
 import numpy as np
 import scipy.ndimage
 import matplotlib.pyplot as plt
+import argparse
 
 
 class ConverterImg():
-    def __init__(self, url):
+    def __init__(self, url, name):
         self.url = url
+        self.name = name
 
     def img_read(self):
         return imageio.imread(self.url)
@@ -32,4 +35,16 @@ class ConverterImg():
         blur_img = self.blur_img(gray_inv)
 
         result = self.dodge(blur_img, gray)
-        plt.imsave("img2.png", result, cmap ="gray", vmin = 0, vmax = 255)
+        plt.imsave("{}.png".format(self.name), result, cmap ="gray", vmin = 0, vmax = 255)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--url', type=str,
+                        help='imgs url')
+    parser.add_argument('-n', '--name', type=str,
+                        help='output file name')
+    args = parser.parse_args()
+
+    conv = ConverterImg(args.url, args.name)
+    conv.run()
